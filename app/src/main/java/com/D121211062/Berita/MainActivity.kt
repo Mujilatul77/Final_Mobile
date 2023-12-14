1e520ad8d38e61da8934495c83cb95f683c46581
Mujilatul77 <nurlia.liia2020@gmail.com>
2023-12-15 00:00:32 +0800

    Initial commit

---
+++ app/src/main/java/com/D121211062/Berita/MainActivity.kt
@@ -0,0 +1,148 @@
+package com.D121211062.Berita
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.D121211062.Berita.ui.theme.D121211062BeritaTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TugasAkhirMobileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column {
+                        CenterAlignedTopAppBar(
+                            modifier = Modifier.background(Color.Blue),  // Change background color here
+                            title = {
+                                Text(
+                                    text = "Popular",
+                                    fontWeight = FontWeight.SemiBold,
+                                )
+                            }
+                        )
+                        val mainViewModel: MainViewModel = viewModel(factory = MainViewModel.Factory)
+                        ListMoviesScreen(mainViewModel.mainUiState)
+                    }
+
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun ListMoviesScreen(mainUiState: MainUiState, modifier: Modifier = Modifier) {
+        when (mainUiState) {
+            is MainUiState.Loading -> CenterText(text = "Loading...")
+            is MainUiState.Error -> CenterText(text = "Something Error")
+            is MainUiState.Success -> MovieList(mainUiState.movies)
+        }
+    }
+
+    @Composable
+    fun CenterText(text: String) {
+        // Wrap the content with a Box to apply the centering modifiers
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp), // Optional padding
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text,
+            )
+        }
+    }
+
+    @Composable
+    fun MovieList(movies: List<Movie>, modifier: Modifier = Modifier) {
+        LazyColumn(modifier = modifier) {
+            items(movies) { movie ->
+                MovieItem(movie = movie)
+            }
+        }
+    }
+
+    @Composable
+    fun MovieItem(movie: Movie) {
+        Box(
+            modifier = Modifier
+                .padding(16.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(8.dp),)
+                .clickable {
+                    val intent = Intent(this, DetailActivity::class.java)
+                    intent.putExtra("MOVIE", movie)
+                    startActivity(intent)
+                }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                // Movie Poster
+                AsyncImage(
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data("https://image.tmdb.org/t/p/original" + movie.backdrop_path)
+                        .crossfade(true)
+                        .build(), contentDescription = movie.title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop
+                )
+
+                // Movie Details
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = movie.title.toString(),
+                    style = MaterialTheme.typography.headlineSmall,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = movie.release_date.toString(),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = movie.overview.toString(), style = MaterialTheme.typography.bodyMedium)
+
+                // Other details like rating, genre, etc. can be added here
+            }
+        }
+    }
+}
