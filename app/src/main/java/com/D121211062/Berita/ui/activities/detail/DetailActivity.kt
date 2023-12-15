79be8a88fe84a34b12217b68de4d8ac13e0ff751
Mujilatul77 <nurlia.liia2020@gmail.com>
2023-12-15 23:38:22 +0800

    -

---
+++ app/src/main/java/com/D121211062/Berita/ui/activities/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.D121211062.Berita.ui.activities.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.D121211062.Berita.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
